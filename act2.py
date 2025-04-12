0a02d316f0e509002415452d54fa4fc9f88a5618
Sauda11 <raeesa_kakti@yahoo.co.in>
2025-04-12 15:28:19 +0530

    c

--- act2.py
+++ act2.py
@@ -1,3 +1,4 @@
+# Program to convert roman to decimal numbers
 
 def roman_to_int(a):      #CD
   roman={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
--- act3.py
+++ act3.py
@@ -1,12 +1,15 @@
-number = int(input("Input number:"))
-s = 0
-temp = number
 
-while temp!=0:
-  digit = temp%10
-  s = s+digit**3
-  temp = temp//10
-if number == s:
-  print(number, "is an armstrong number ")
-else:
-  print(number, "is not an armstrong number ")
+def print_factors(number):
+
+  print("The factors of",number,"are:")
+
+  for i in range(1, number + 1):
+
+    if number % i== 0:
+
+      print(i)
+
+number = int(input("Enter your number to find it's factors: "))
+
+print_factors(number)
+
